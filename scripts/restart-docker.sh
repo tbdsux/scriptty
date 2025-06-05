ec78f89dbde158f70b9245c8a4d84aa0f14da35a
Joshue Abance <iamcoderx@gmail.com>
2025-06-06 04:23:12 +0800

    build: update restart script

--- scripts/restart-docker.sh
+++ scripts/restart-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Remove specific volumes
+# Stop containers
+docker compose down
+
+# Remove specific laravel volumes
+# We need to remove these volumes to ensure that the latest assets are built
+# For some reason, it is not enough to just rebuild the containers, new assets are not picked up
 docker volume rm scriptty_laravel-public-assets scriptty_laravel-storage-production
 
 # Rebuild and start containers
